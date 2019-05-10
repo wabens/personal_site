2153824d51af9fd77aa5fb8bf6cae55dc58572b7
wabens <waltkbenson@gmail.com>
2019-05-10 15:33:23 -0500

    basic nav bar with buttons and some layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Nav from '../Nav/Nav'
 
 function App() {
   return (
-    <div className="App">
- 
+    <div>
+      <Nav/>
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import './Nav.css';
+
+import Button from '@material-ui/core/Button';
+
 
 class Nav extends Component {
 
 
     render(){
         return (
-            <section>
-
+            <section className = "nav">
+                <div className = "button-box">
+                    <Button>About</Button>
+                    <Button>Projects</Button>
+                    <Button>Resume</Button>
+                    <Button>Contact</Button>
+                </div>
             </section>
         )
     }
