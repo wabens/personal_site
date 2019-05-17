78c84df201abbc39690167e8e0eaa6a9a7677909
wabens <waltkbenson@gmail.com>
2019-05-17 13:57:17 -0500

    Updates

---
+++ precache-manifest.99152608003d61bb61102415f00f1b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb81e5b5fac80e6366ecf4043e83cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78557d87484c0d538b51",
+    "url": "/static/css/main.552bd9fd.chunk.css"
+  },
+  {
+    "revision": "fe9f80366a52d1366747",
+    "url": "/static/js/2.e2b3b1a4.chunk.js"
+  },
+  {
+    "revision": "78557d87484c0d538b51",
+    "url": "/static/js/main.65bee076.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
